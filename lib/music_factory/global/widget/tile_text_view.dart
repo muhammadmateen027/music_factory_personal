b70f5db211dafcb40e4ddae8efcc4f927d884e75
Muhammad Mateen <muhammadmateen@airasia.com>
2021-07-22 21:48:16 +0800

    Create a text view

--- lib/music_factory/global/widget/tile_text_view.dart
+++ lib/music_factory/global/widget/tile_text_view.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
 class TileTextView extends StatelessWidget {
-  const TileTextView({Key? key, required this.title}) : super(key: key);
-  final String title;
+  const TileTextView({
+    Key? key,
+    required this.title,
+    this.textStyle = const TextStyle(),
+  }) : super(key: key);
+  final String? title;
+  final TextStyle? textStyle;
 
   @override
   Widget build(BuildContext context) {
-    return Text(title);
+    if (title == null) {
+      return Container();
+    }
+
+    return Text(title!, style: textStyle);
   }
 }
