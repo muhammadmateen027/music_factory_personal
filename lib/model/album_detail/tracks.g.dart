1083b4238326c048b72be5b219d61a3765d4b160
Muhammad Mateen <muhammadmateen@airasia.com>
2021-07-23 12:04:07 +0800

    Update generated file to handle dynamic objects

--- lib/model/album_detail/tracks.g.dart
+++ lib/model/album_detail/tracks.g.dart
@@ -7,11 +7,22 @@ part of 'tracks.dart';
 // **************************************************************************
 
 Tracks _$TracksFromJson(Map<String, dynamic> json) {
-  return Tracks(
-    track: (json['track'] as List<dynamic>?)
-        ?.map((e) => Track.fromJson(e as Map<String, dynamic>))
-        .toList(),
-  );
+
+  if (json['track'] == null || json['track'] == '') {
+    return Tracks();
+  }
+
+  if (json['track'] is List<dynamic>?) {
+    return Tracks(
+      track: (json['track'] as List<dynamic>?)
+          ?.map((e) => Track.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+  }
+
+  var tracks = <Track>[];
+  json.forEach((key, value) => tracks.add(Track.fromJson(value)));
+  return Tracks(track: tracks);
 }
 
 Map<String, dynamic> _$TracksToJson(Tracks instance) => <String, dynamic>{
