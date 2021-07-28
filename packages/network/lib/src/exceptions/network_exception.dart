b8b81c0abb5f499466163d63b4c2b8a2c47a9054
Muhammad Mateen <muhammadmateen@airasia.com>
2021-07-28 21:00:01 +0800

    Update network exception

--- packages/network/lib/src/exceptions/network_exception.dart
+++ packages/network/lib/src/exceptions/network_exception.dart
@@ -3,17 +3,16 @@ import 'package:dio/dio.dart';
 /// We will have class that can implement exceptions
 class NetworkException implements Exception {
   /// Pass the DioError in the constructor
-  const NetworkException(DioError this.error);
+  const NetworkException(this.dioError);
 
   /// Variable will be used in the class
-  final DioError? error;
+  final DioError? dioError;
   /// If the error type is not matched, then we will show default error
   final String defaultErrorDetail = 'Something went wrong. Please try again.';
 
   /// parse the error in this function and get the details
   String getDetail() {
     var detail = defaultErrorDetail;
-
 
     return detail;
   }
