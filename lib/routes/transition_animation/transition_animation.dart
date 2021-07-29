789879bd467d4e56558e9ab71f3af9e26fb8d39a
Muhammad Mateen <muhammadmateen@airasia.com>
2021-07-29 08:45:39 +0800

    Update app transition

--- lib/routes/transition_animation/transition_animation.dart
+++ lib/routes/transition_animation/transition_animation.dart
@@ -3,163 +3,40 @@ import 'package:flutter/material.dart';
 class PageTransition {
   PageTransition._();
 
-  static PageRouteBuilder slide(Widget page) {
+  static PageRouteBuilder slide(Widget page) =>
+      _baseSlide(page, const Offset(0.0, 1.0), Offset.zero, Curves.ease);
+
+  static PageRouteBuilder slideRight(Widget page) =>
+      _baseSlide(page, const Offset(-1, 0), Offset.zero);
+
+  static PageRouteBuilder slideUpRoute(Widget page) =>
+      _baseSlide(page, const Offset(0, 1), Offset.zero);
+
+  static PageRouteBuilder slideDownRoute(Widget page) =>
+      _baseSlide(page, const Offset(0, -1.0), Offset.zero);
+
+  static PageRouteBuilder slideBottomRight(Widget page) =>
+      _baseSlide(page, const Offset(1, 1), Offset.zero);
+
+  static PageRouteBuilder slideLeft(Widget page) =>
+      _baseSlide(page, const Offset(1, 0), const Offset(0, 0));
+
+  static PageRouteBuilder _baseSlide(
+      Widget page, Offset begin, Offset end, [Curve? curve]
+      )
+  {
     return PageRouteBuilder(
       pageBuilder: (context, animation, secondaryAnimation) => page,
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(0.0, 1.0);
-        const end = Offset.zero;
-        const curve = Curves.ease;
-
-        final tween = Tween(begin: begin, end: end).chain(
+        final tween = curve == null
+            ? Tween(begin: begin, end: end)
+            : Tween(begin: begin, end: end).chain(
           CurveTween(curve: curve),
         );
 
         return SlideTransition(
           position: animation.drive(tween),
           child: child,
-        );
-      },
-    );
-  }
-
-  static PageRouteBuilder slideRight(Widget page) {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(-1, 0);
-        const end = Offset.zero;
-
-        final tween = Tween(begin: begin, end: end);
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-      },
-    );
-  }
-
-  static PageRouteBuilder slideUpRoute(Widget page) {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(0, 1);
-        const end = Offset.zero;
-
-        final tween = Tween(begin: begin, end: end);
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-      },
-    );
-  }
-
-  static PageRouteBuilder slideDownRoute(Widget page) {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(0, -1.0);
-        const end = Offset.zero;
-
-        final tween = Tween(begin: begin, end: end);
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-      },
-    );
-  }
-
-  static PageRouteBuilder slideBottomRight(Widget page) {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(1, 1);
-        const end = Offset.zero;
-
-        final tween = Tween(begin: begin, end: end);
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-      },
-    );
-  }
-
-  static PageRouteBuilder slideLeft(Widget page) {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = Offset(1, 0);
-        const end = Offset(0, 0);
-
-        final tween = Tween(begin: begin, end: end);
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-      },
-    );
-  }
-
-  static PageRouteBuilder scale(Widget page) {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        const begin = 0.0;
-        const end = 1.0;
-
-        final tween = Tween(begin: begin, end: end);
-
-        return ScaleTransition(
-          scale: animation.drive(tween),
-          child: child,
-        );
-      },
-    );
-  }
-
-  static PageRouteBuilder size(Widget page) {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => page,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return Align(
-          child: SizeTransition(
-            sizeFactor: animation,
-            child: child,
-          ),
-        );
-      },
-    );
-  }
-
-  static PageRouteBuilder enterExit(Widget enterPage, Widget exitPage) {
-    return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => enterPage,
-      transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return Stack(
-          children: <Widget>[
-            SlideTransition(
-              position: Tween<Offset>(
-                begin: const Offset(0.0, 0.0),
-                end: const Offset(-1.0, 0.0),
-              ).animate(animation),
-              child: exitPage,
-            ),
-            SlideTransition(
-              position: Tween<Offset>(
-                begin: const Offset(1.0, 0.0),
-                end: Offset.zero,
-              ).animate(animation),
-              child: enterPage,
-            )
-          ],
         );
       },
     );
